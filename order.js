8d4201d3c825e1b7e0234e211f20e1047a8ed36f
SunshineMocha <127298037+SunshineMocha@users.noreply.github.com>
2023-04-03 09:28:56 +0200

    updated order.js

--- order.js
+++ order.js
@@ -9,6 +9,6 @@ class Order{
         return total;
     }
     toString(){
-        return `----------\nProduct: ${this.product} \nQuantity: ${this.quantity}\nUnit Cost: ${this.unitPrice}€\nTotal: ${this.totalPrice()}€\n`;
+        return `----------\nProduct: ${this.product} \nQuantity: ${this.quantity}\nUnit Cost: ${this.unitPrice.toFixed(2)}€\nTotal: ${this.totalPrice().toFixed(2)}€\n`;
     }
 }
